6921b272ae1c2b279e6fd27f722062c4565930f3
Lucas Oliveira <lucas.o@live.com>
2024-01-25 22:05:32 -0300

    Added path to return image urls as json

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 import numpy as np
 
@@ -12,28 +12,49 @@ embeddings = np.load("data/embeddings.npy")
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET", "POST"])
-def get_index():
+def search_images_text_and_urls(search_query, top_k=20):
+    """
+    Given a search query, return the top k (default 20) most similar images and their urls.
+    """
+
+    if search_query.strip() == "":
+        return []
+
+    # Get embeddings for search query
+    query_emb = get_embeddings([search_query])
+
+    # Compute cosine similarity between search query and all other embeddings
+    # This may be slow for large datasets, for ours its fine.
+    sim = query_emb.dot(embeddings.T)
+
+    # Get top 20 most similar images
+    sim_indices = (-sim[0]).argsort()[:top_k]
+
     img_urls = []
+
+    # Get the urls and texts for the most similar images and return them
+    for i in sim_indices:
+        for url in urls[i]:
+            img_urls.append((texts[i], url))
+
+    return img_urls
+
+
+@app.route('/image_urls', methods=["GET"])
+def get_image_urls():
 
     search_query = request.args.get("query", "")
 
-    # Ensure search query is not empty
-    if search_query.strip() != "":
+    img_urls = search_images_text_and_urls(search_query)
 
-        # Get embeddings for search query
-        query_emb = get_embeddings([search_query])
+    return jsonify(img_urls)
 
-        # Compute cosine similarity between search query and all other embeddings
-        # This may be slow for large datasets, for ours its fine.
-        sim = query_emb.dot(embeddings.T)
 
-        # Get top 20 most similar images
-        sim_indices = (-sim[0]).argsort()[:20]
+@app.route('/', methods=["GET"])
+def get_index():
+    
+    search_query = request.args.get("query", "")
 
-        # Get the urls and texts for the top 20 most similar images and display them
-        for i in sim_indices:
-            for url in urls[i]:
-                img_urls.append((texts[i], url))
+    img_urls = search_images_text_and_urls(search_query)
 
     return render_template("index.html", img_urls=img_urls)
